2c07f544a5ba8e4f67aff4fcb96d359d5790eeee
Booorgi <bartoszmarciniak@op.pl>
2026-09-05 13:26:33 +0200

    Fix rotation adjustment for line geometry

--- frontend/components/ModelViewer.js
+++ frontend/components/ModelViewer.js
@@ -112,7 +112,8 @@ export default function ModelViewer({
         lineGeo.rotateX(-Math.PI / 2);
 
         // B. Obrót w poziomie wokół osi pionowej Y dopasowujący orientację podpór do bryły
-        lineGeo.rotateY(Math.PI);
+        // B. Obrót w poziomie wokół osi pionowej Y dopasowujący orientację podpór do bryły
+        lineGeo.rotateY(-Math.PI / 2);
 
         // C. Centrowanie w poziomie i wyrównanie podstawy do poziomu stołu (Y = 0)
         lineGeo.computeBoundingBox();
