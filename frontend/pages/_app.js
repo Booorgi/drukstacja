938fb8341013b137b49b03b7e5724a978ea6f39f
Booorgi <twoj-email-z-githuba@gmail.com>
2026-09-05 15:05:39 +0200

    Uzycie Tailwind CDN i usuniecie dyrektyw z css

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
